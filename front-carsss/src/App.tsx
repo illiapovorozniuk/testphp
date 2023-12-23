7b2421cdba00a5a51d21be17ecd2dcd934f93f84
Illia <ipz211_piv@student.ztu.edu.ua>
2023-12-23 11:08:15 +0200

    initial back and front

---
+++ front-carsss/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <button>normal button</button>
+      <Button>Bootstrap button</Button>
+    </>
+  );
+}
+
+export default App;
